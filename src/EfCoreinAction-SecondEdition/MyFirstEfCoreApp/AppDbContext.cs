eec273902d33fe2380f9f9e1a6a7a57f4166afde
josenerydev <josenerydev@gmail.com>
2021-07-26 20:42:34 -0300

    Final Commit MyFirstEFCoreApp

---
+++ src/EfCoreinAction-SecondEdition/MyFirstEfCoreApp/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyFirstEfCoreApp
+{
+    public class AppDbContext : DbContext
+    {
+        private const string ConnectionString =
+            @"Server=(localdb)\mssqllocaldb;
+            Database=MyFirstEfCoreDb;
+            Trusted_Connection=True";
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
---
+++ src/EfCoreinAction-SecondEdition/MyFirstEfCoreApp/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyFirstEfCoreApp
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime PublishedOn { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
